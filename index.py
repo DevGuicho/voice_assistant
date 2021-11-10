2d1142caf5016e19ef060563987e7435ddf31c28
Luis Vazquez <luvazpa@gmail.com>
2021-11-10 14:41:44 -0600

    Questions information added without context

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
 import speech_recognition as sr
 import pyttsx3
+import wikipedia
+
+wikipedia.set_lang("es")
 
 name = 'alexa'
 
@@ -12,7 +15,6 @@ engine.setProperty('voice',voices[0].id)
 
 
 rate = engine.getProperty('rate')   # getting details of current speaking rate
-print (rate)                        #printing current voice rate
 engine.setProperty('rate', 140) 
 
 def talk(text):
@@ -31,27 +33,137 @@ def listen():
     talk('No entendí eso')
     return "Google Speech Recognition could not understand audio"
   
+
+def oneAnswer(rec):
+  if 'cuántos huesos tiene el cuerpo humano' in rec:
+    talk('El cuerpo humano adulto tiene nada menos que 206 huesos')
+    return True
+  elif 'cuántos dientes tiene una persona adulta' in rec:
+    talk('32 piezas dentales es la cantidad normal para un adulto')
+    return True
+  elif 'cuál es el porcentaje de agua en el cuerpo humano' in rec:
+    talk('El cuerpo humano está compuesto en un 60 por ciento de agua, el cerebro se compone en un 70 por ciento de agua, la sangre en un 80 por ciento y los pulmones se componen en un 90 por ciento de agua.')
+    return True
+  elif 'cuántos músculos tenemos' in rec:
+    talk('El cuerpo humano contiene aproximadamente 650 músculos estriados.')
+    return True
+  elif 'cuántos órganos tiene el cuerpo humano' in rec:
+    talk('El cuerpo humano tiene 21 órganos')
+    return True
+  
+  return False
   
 
+def twoAnswers(rec):
+  if 'qué es el acné' in rec:
+    talk('El acne es una enfermedad cutánea que suele aparecer a partir de los 12 años de edad, sobre todo en la cara, y que, si no hay complicaciones graves, solo se desarrolla durante la pubertad y desaparece hacia los 18 o 20 años.')
+    return True
+  elif 'cómo prevenir el acné' in rec:
+    talk('Lávate la cara dos veces al día (no más) con agua tibia y un jabón suave fabricado específicamente para personas con acné. Masajéate suavemente la cara describiendo movimientos circulares. No te frotes la cara.')
+    return True
+  elif 'qué es la menopausia' in rec:
+    talk('Es un proceso biológico natural que marca el final de los ciclos menstruales. Se diagnostica después de que transcurren doce meses sin que tengas un período menstrual.')
+    return True
+  elif 'a qué edad se da la menopausia' in rec:
+    talk('La menopausia puede producirse entre los 40 y 50 años, pero la edad promedio es a los 51 años.')
+    return True
+  elif 'qué son los piojos' in rec:
+    talk('Los piojos son pequeños insectos que viven en la cabeza de las personas. Los piojos adultos tienen el tamaño de las semillas de sésamo. Los huevos, llamados liendres, son incluso más pequeños, aproximadamente del tamaño de una escama de caspa.')
+    return True
+  elif 'cómo se contagian los piojos' in rec:
+    talk('Los piojos se mueven arrastrándose, porque no pueden saltar ni volar. Se contagian a través del contacto cercano de persona a persona. En raras ocasiones, se pueden contagiar al compartir objetos personales como sombreros o cepillos para el cabello. La higiene personal y la limpieza no se relacionan con tener piojos.')
+    return True
+  elif 'qué es la varicela' in rec:
+    talk('La varicela es una infección causada por el virus de la varicela-zóster. Causa una erupción en la piel con picazón y pequeñas ampollas con líquido. La varicela es muy contagiosa para personas que no han tenido la enfermedad antes o no se han vacunado. ')
+    return True
+  elif 'qué pasa si a un adulto le da varicela' in rec:
+    talk('Puede causar: deshidratación, neumonía, problemas de sangrado, infección o inflamación cerebral, infecciones bacterianas de la piel, infecciones del torrente sanguíneo (septicemia), síndrome de shock tóxico, infecciones de los huesos, infecciones de las articulaciones, o la muerte.')
+    return True
+  elif 'qué es el aparato digestivo' in rec:
+    talk('Conjunto de órganos que procesan los alimentos y los líquidos para descomponerlos en sustancias que el cuerpo usa como fuente de energía, o para el crecimiento y la reparación de tejidos.')
+    return True
+  elif 'qué órganos conforman el aparato digestivo' in rec:
+    talk('El aparato digestivo está formado por el tracto gastrointestinal, también llamado tracto digestivo, y el hígado, el páncreas y la vesícula biliar.')
+    return True
+
+  return False
+
+def threeAnswers(rec):
+  if 'qué son enfermedades cardiovasculares' in rec:
+    talk('Las enfermedades cardiovasculares (ECV) son un grupo de desórdenes del corazón y de los vasos sanguíneos.')
+    return True
+  elif 'cuáles son los síntomas comunes de las enfermedades cardiovasculares' in rec:
+    talk('Molestias en el pecho, molestias en los brazos, hombro izquierdo, mandíbula o espalda')
+    return True
+  elif 'cuáles son los principales factores de riesgo de las sv' in rec:
+    talk('Las causas más importantes son una dieta malsana, la inactividad física, el consumo de tabaco y el consumo nocivo de alcohol')
+    return True
+  elif 'qué son las enfermedades cerebrovasculares' in rec:
+    talk('Son isquémicas y ocurren cuando las arterias que llevan sangre al cerebro se hacen angostas o se bloquean, causando una disminución severa o interrupción del flujo sanguíneo, conocida como isquemia.')
+    return True
+  elif 'cuáles son los síntomas de las enfermedades cerebrovasculares' in rec:
+    talk('Alteración repentina de la visión en un ojo o ambos, Pérdida repentina de la fuerza en un brazo, una pierna o ambos. Sensación de hormigueo en la cara, brazo o pierna. Aparición repentina de: Problemas para hablar y/o entender lo que se escucha, acompañada por balbuceo. Desequilibrio o inestabilidad. Dolor de cabeza.')
+    return True
+  elif 'cuáles son los factores de riesgo de las enfermedades cerebrovasculares' in rec:
+    talk('El sexo. Se presenta con mayor frecuencia en hombres que en mujeres. Alcoholismo. Tabaquismo. Inactividad física. Obesidad. Presión arterial alta. Diabetes Mellitus. Niveles de colesterol elevados.')
+    return True
+  elif 'qué es el coronavirus' in rec:
+    talk('Los coronavirus son una familia de virus que causan enfermedades (desde el resfriado común hasta enfermedades respiratorias más graves) y circulan entre humanos y animales.')
+    return True
+  elif 'cómo se transmite el coronavirus' in rec:
+    talk('Los coronavirus humanos se transmiten de una persona infectada a otras: a través de las gotículas que expulsa un enfermo al toser y estornudar ,al tocar o estrechar la mano de una persona enferma, un objeto o superficie contaminada con el virus y luego llevarse las manos sucias a boca, nariz u ojos.')
+    return True
+  elif 'cuándo debo acudir a recibir atención médica' in rec:
+    talk('Una persona debe sospechar de COVID-19 cuando presenta al menos dos de los siguientes síntomas: Tos / Estornudos, Fiebre, Dolor de cabeza.')
+    return True
+  elif 'qué es el sistema circulatorio' in rec:
+    talk('El sistema circulatorio se encarga de bombear, transportar y distribuir la sangre por todo el cuerpo.')
+    return True
+  elif 'cómo está integrado el sistema circulatorio' in rec:
+    talk('Se integra con el corazón y los vasos sanguíneos: arterias, venas y capilares.')
+    return True
+  elif 'qué es el corazón' in rec:
+    talk('Es una bomba muscular y se considera el centro del sistema circulatorio.')
+    return True
+  elif 'qué es el cáncer' in rec:
+    talk('El cáncer puede desarrollarse en cualquier parte del cuerpo. Se origina cuando las células crecen sin control y sobrepasan en número a las células normales. Esto hace que al cuerpo le resulte difícil funcionar de la manera que debería hacerlo')
+    return True
+  elif 'cuál es el tratamiento para el cáncer' in rec:
+    talk('La mayoría de las personas reciben una combinación de tratamientos, como cirugía con quimioterapia o radioterapia.')
+    return True
+  elif 'qué es quimioterapia' in rec:
+    talk('Tratamiento con medicamentos para interrumpir la formación de células cancerosas, ya sea mediante su destrucción o al impedir su multiplicación. La quimioterapia se administra por la boca, en inyección, por infusión o sobre la piel, según el tipo de cáncer y el estado en que este se encuentra. Se administra sola o con otros tratamientos como cirugía, radioterapia o terapia biológica.')
+    return True
+
+  return False
+
 def options(rec):
-  flag = True
   print(rec)
-  if 'apaga te' in rec:
+  if oneAnswer(rec):
+    return True
+  elif twoAnswers(rec):
+    return True
+  elif threeAnswers(rec):
+    return True
+  elif 'apaga te' in rec:
     talk('Hasta pronto')  
-    flag = False
-  elif 'qué es linux' in rec:
-    talk('Linux es un kernel de código abierto creado por Linus Torvalds')
-  elif 'cuántas temporadas tiene doctor house' in rec:
-    talk('Dr House: Diagnóstico Médico cuenta con ocho temporadas emitidas entre 2004 y 2012')
+    return False  
+  elif 'apagáte'  in rec:
+    talk('Hasta pronto')  
+    return False  
+  elif 'hola' in rec:
+    talk('Hola, ¿En que te puedo ayudar?')  
+    return True  
   else:
     talk('No entendí eso')
-  return flag
+  return True
   
 flag = True
 while flag:
   rec = listen()
   if name in rec:
     flag = options(rec)
-  
+
+
 
 engine.stop()
